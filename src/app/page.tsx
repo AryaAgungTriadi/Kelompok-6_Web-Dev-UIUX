d0bcc7e85e1f25c3bdd8c97309caa9d1ed60e0f1
AryaAgungTriadi <aryamojangbandung@gmail.com>
2026-04-20 20:39:19 +0700

    add distribution section and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import react from "react";
+import React from "react";
 import Hero from "@/components/Hero";
 import { WhyChooseUs } from "@/components/sections/WhyChooseUs";
 import { JoinUsers } from "@/components/sections/JoinUsers";
+import { Distribution } from "@/components/sections/Distribution";
+import { Footer } from "@/components/sections/Footer";
 
 export default function Home() {
   return (
@@ -9,9 +11,8 @@ export default function Home() {
       <Hero />
       <WhyChooseUs />
       <JoinUsers />
-      {}
-      
-
+      <Distribution />
+      <Footer />
     </main>
   );
 }
