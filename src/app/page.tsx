7d75f7e402fa79ecc34ecfaccf3062bddaba0781
Ilhamaulnaaa <ilhamaulnaaa@gmail.com>
2026-04-20 13:34:52 +0700

    Navbar & Hero

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import react from "react";
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      {}
+
+      
+    </main>
+  );
+}
